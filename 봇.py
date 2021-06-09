8abf6e08dcf94baee5621c5e65f97e45a3c7db3b
Tanat <85154556+TANAT96564@users.noreply.github.com>
2021-06-09 23:43:43 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -11,6 +11,9 @@ async def on_ready():
     print(client.user.id)
     await client.change_presence(game=discord.Game(name='', type=1))
 
+now = datetime.datetime.now()
+time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분"
+
 @client.event
 async def on_message(message):
     if message.author.bot:
@@ -19,5 +22,9 @@ async def on_message(message):
     if message.content.startswith('알파야'):
         channel = message.channel
         await channel.send('ㅇ?')
+
+    if message.content.startswith('알파야 현재 시간'):
+        channel = message.channel
+        await channel.send(time)
         
 client.run(access_token)
