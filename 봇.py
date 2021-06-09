877ed104e9a0b277142f4deee6370e794e9e65c5
sounghyun890 <66344807+sounghyun890@users.noreply.github.com>
2021-06-09 23:01:30 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -10,14 +10,12 @@ async def on_ready():
     print(client.user.id)
     print("================")
 
-# 봇이 특정 메세지를 받고 인식하는 코드
 @client.event
 async def on_message(message):
-    # 메세지를 보낸 사람이 봇일 경우 무시한다
     if message.author.bot:
         return None
 
-    if message.content.startswith('!안녕'):
+    if message.content.startswith('안녕'):
         channel = message.channel
         await channel.send('반가워!')
         
