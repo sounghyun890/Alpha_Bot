d5becfebcf2acfd480845356c179e59ce9a1aa2e
sounghyun890 <66344807+sounghyun890@users.noreply.github.com>
2021-06-09 23:18:26 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -2,9 +2,14 @@ import discord
 import os
 
 client = discord.Client()
+access_token = os.environ["BOT_TOKEN"]
 
 @client.event
 async def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    await client.change_presence(game=discord.Game(name='', type=1))
 
 @client.event
 async def on_message(message):
@@ -15,5 +20,4 @@ async def on_message(message):
         channel = message.channel
         await channel.send('반가워!')
         
-access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
