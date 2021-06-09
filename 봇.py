4f5f6f1f66fdc1b5603c7754a6992b2ee5b25519
Tanat <85154556+TANAT96564@users.noreply.github.com>
2021-06-10 07:46:11 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -12,10 +12,11 @@ async def on_ready():
     print(client.user.id)
     await client.change_presence(game=discord.Game(name='', type=1))
 
+
+@client.event
 now = datetime.datetime.now()
 time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분"
 
-@client.event
 async def on_message(message):
     if message.author.bot:
         return None
