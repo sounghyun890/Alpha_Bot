f6f26c33d7ef809969b4a9e07aa3157f5b2bae17
sounghyun890 <66344807+sounghyun890@users.noreply.github.com>
2021-06-09 23:03:34 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -5,10 +5,6 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("다음으로 로그인합니다")
-    print(client.user.name)
-    print(client.user.id)
-    print("================")
 
 @client.event
 async def on_message(message):
