db9b50c542c961477d28b80f3ebba54d82fc5c51
sounghyun890 <66344807+sounghyun890@users.noreply.github.com>
2021-06-10 09:28:57 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -29,7 +29,7 @@ async def on_message(message):
 
     if message.content.startswith('!현재 시간'):
         now = datetime.datetime.now()
-        time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {시간}시 {str(now.minute)}분"
+        time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분"
         channel = message.channel
         await channel.send(time)
         
