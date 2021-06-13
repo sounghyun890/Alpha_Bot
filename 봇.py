b8e7cdb30c61aff4ef5014572f32348ef497074d
Tanat <85154556+TANAT96564@users.noreply.github.com>
2021-06-13 17:33:09 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -1,20 +1,7 @@
-import discord
 import asyncio
-import random
-import openpyxl
-from discord import Member
-from discord.ext import commands
-import youtube_dl
-from urllib.request import urlopen, Request
-import urllib
-import urllib.request
-import bs4
-import os
-import sys
-import json
-from selenium import webdriver
-import time
+import discord,os
 import datetime
+import random
 
 client = discord.Client()
 access_token = os.environ["BOT_TOKEN"]
@@ -43,21 +30,16 @@ async def on_ready():
         
 now = datetime.datetime.now()
 time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분"
-
 @client.event
 async def on_message_delete(message):#메세지가 삭제 되면
     if message.author.bot:return
     
     if message.guild.id ==653083797763522580:return
-
-
     channel = client.get_channel(849536197273059338)
     embed = discord.Embed(title=f"삭제됨", description=f"유저 : {message.author.display_name} \n유저ID : {message.author} \n서버 : {message.guild.name} \n채널 : {message.channel.mention}", color=0xFF0000)
     embed.add_field(name="삭제된 내용", value=f"내용 : {message.content}", inline=False)
     embed.set_footer(text=f"TNS 봇 | {time}")
     await channel.send(embed=embed)
-
-
 @client.event    
 async def on_message_edit(before, after):#메세지 수정 되면(작동 안함)
     if message.author.bot:return
@@ -67,17 +49,14 @@ async def on_message_edit(before, after):#메세지 수정 되면(작동 안함)
     embed.add_field(name="수정 후 내용", value=after.content, inline=True)
     embed.set_footer(text=f"{before.guild.name} | {time}")
     await channel.send(embed=embed)
-
 # 봇이 새로운 메시지를 수신했을때 동작되는 코드입니다.
 @client.event
 async def on_message(message):
     if message.author.bot:
         return None
    
-
     id = message.author.id
     channel = message.channel
-
     if message.content == "!도움":
         embed = discord.Embed(title = "TNS 봇의 도움말", description = '''
         욕 검열 봇입니다
@@ -87,8 +66,6 @@ TNS봇은 삭제 된 내용을 로그서버로 전송하고 있습니다
 봇 로그 보러가기 https://discord.gg/hFryJ4zYyw''', color = 0x08FFFA)
         await message.author.send(embed = embed)
         await message.delete()
-
-
     if message.author.bot:
     
         await message.author.send(embed = embed) # message.channel.send를 message.author.send로
@@ -114,8 +91,6 @@ TNS봇은 삭제 된 내용을 로그서버로 전송하고 있습니다
         await message.delete() 
         await asyncio.sleep(7)
         await a.delete()
-
-
     #욕설
     bad1 = message_content.find("씨발")
     bad1 = bad1 + message_content.find("닥쳐")
@@ -158,8 +133,6 @@ TNS봇은 삭제 된 내용을 로그서버로 전송하고 있습니다
         await message.delete() 
         await asyncio.sleep(7)
         await a.delete()
-
-
     #폐드립
     bad2 = message_content.find("ㄴㄱㅁ")
     bad2 = bad2 + message_content.find("느금마")
@@ -174,7 +147,6 @@ TNS봇은 삭제 된 내용을 로그서버로 전송하고 있습니다
         await message.delete() 
         await asyncio.sleep(7)
         await a.delete()
-
     
     #섹드립
     bad3 = message_content.find("좇")
@@ -224,8 +196,6 @@ TNS봇은 삭제 된 내용을 로그서버로 전송하고 있습니다
         await message.delete() 
         await asyncio.sleep(7)
         await a.delete()
-
-
     #비하발언
     bad5 = message_content.find("따까리")
     bad5 = bad5 + message_content.find("찐따")
@@ -238,7 +208,6 @@ TNS봇은 삭제 된 내용을 로그서버로 전송하고 있습니다
         await asyncio.sleep(7)
         await a.delete()
     await bot.process_commands(messsage)
-
     #초성 욕설 bad 12개
     #욕설 bad1 34개
     #폐드립 bad2 6개
@@ -248,7 +217,6 @@ TNS봇은 삭제 된 내용을 로그서버로 전송하고 있습니다
 @client.event  
 async def on_message(message):
      if message.content.startswith('!주사위'):
-
         randomNum = random.randrange(1, 7) # 1~6까지 랜덤수
         print(randomNum)
         if randomNum == 1:
@@ -263,88 +231,5 @@ async def on_message(message):
             await message.channel.send("5")
         if randomNum ==6:
             await message.channel.send("6")
-            
-@client.event  
-async def on_message(message):            
-    if message.content.startswith("!날씨"):
-        learn = message.content.split(" ")
-        location = learn[1]
-        enc_location = urllib.parse.quote(location+'날씨')
-        hdr = {'User-Agent': 'Mozilla/5.0'}
-        url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
-        print(url)
-        req = Request(url, headers=hdr)
-        html = urllib.request.urlopen(req)
-        bsObj = bs4.BeautifulSoup(html, "html.parser")
-        todayBase = bsObj.find('div', {'class': 'main_info'})
 
-        todayTemp1 = todayBase.find('span', {'class': 'todaytemp'})
-        todayTemp = todayTemp1.text.strip()  # 온도
-        print(todayTemp)
-
-        todayValueBase = todayBase.find('ul', {'class': 'info_list'})
-        todayValue2 = todayValueBase.find('p', {'class': 'cast_txt'})
-        todayValue = todayValue2.text.strip()  # 밝음,어제보다 ?도 높거나 낮음을 나타내줌
-        print(todayValue)
-
-        todayFeelingTemp1 = todayValueBase.find('span', {'class': 'sensible'})
-        todayFeelingTemp = todayFeelingTemp1.text.strip()  # 체감온도
-        print(todayFeelingTemp)
-
-        todayMiseaMongi1 = bsObj.find('div', {'class': 'sub_info'})
-        todayMiseaMongi2 = todayMiseaMongi1.find('div', {'class': 'detail_box'})
-        todayMiseaMongi3 = todayMiseaMongi2.find('dd')
-        todayMiseaMongi = todayMiseaMongi3.text  # 미세먼지
-        print(todayMiseaMongi)
-
-        tomorrowBase = bsObj.find('div', {'class': 'table_info weekly _weeklyWeather'})
-        tomorrowTemp1 = tomorrowBase.find('li', {'class': 'date_info'})
-        tomorrowTemp2 = tomorrowTemp1.find('dl')
-        tomorrowTemp3 = tomorrowTemp2.find('dd')
-        tomorrowTemp = tomorrowTemp3.text.strip()  # 오늘 오전,오후온도
-        print(tomorrowTemp)
-
-        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
-        tomorrowMoring1 = tomorrowAreaBase.find('div', {'class': 'main_info morning_box'})
-        tomorrowMoring2 = tomorrowMoring1.find('span', {'class': 'todaytemp'})
-        tomorrowMoring = tomorrowMoring2.text.strip()  # 내일 오전 온도
-        print(tomorrowMoring)
-
-        tomorrowValue1 = tomorrowMoring1.find('div', {'class': 'info_data'})
-        tomorrowValue = tomorrowValue1.text.strip()  # 내일 오전 날씨상태, 미세먼지 상태
-        print(tomorrowValue)
-
-        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
-        tomorrowAllFind = tomorrowAreaBase.find_all('div', {'class': 'main_info morning_box'})
-        tomorrowAfter1 = tomorrowAllFind[1]
-        tomorrowAfter2 = tomorrowAfter1.find('p', {'class': 'info_temperature'})
-        tomorrowAfter3 = tomorrowAfter2.find('span', {'class': 'todaytemp'})
-        tomorrowAfterTemp = tomorrowAfter3.text.strip()  # 내일 오후 온도
-        print(tomorrowAfterTemp)
-
-        tomorrowAfterValue1 = tomorrowAfter1.find('div', {'class': 'info_data'})
-        tomorrowAfterValue = tomorrowAfterValue1.text.strip()
-
-        print(tomorrowAfterValue)  # 내일 오후 날씨상태,미세먼지
-
-        embed = discord.Embed(
-            title=learn[1]+ ' 날씨 정보',
-            description=learn[1]+ '날씨 정보입니다.',
-            colour=discord.Colour.gold()
-        )
-        embed.add_field(name='현재온도', value=todayTemp+'˚', inline=False)  # 현재온도
-        embed.add_field(name='체감온도', value=todayFeelingTemp, inline=False)  # 체감온도
-        embed.add_field(name='현재상태', value=todayValue, inline=False)  # 밝음,어제보다 ?도 높거나 낮음을 나타내줌
-        embed.add_field(name='현재 미세먼지 상태', value=todayMiseaMongi, inline=False)  # 오늘 미세먼지
-        embed.add_field(name='오늘 오전/오후 날씨', value=tomorrowTemp, inline=False)  # 오늘날씨 # color=discord.Color.blue()
-        embed.add_field(name='**----------------------------------**',value='**----------------------------------**', inline=False)  # 구분선
-        embed.add_field(name='내일 오전온도', value=tomorrowMoring+'˚', inline=False)  # 내일오전날씨
-        embed.add_field(name='내일 오전날씨상태, 미세먼지 상태', value=tomorrowValue, inline=False)  # 내일오전 날씨상태
-        embed.add_field(name='내일 오후온도', value=tomorrowAfterTemp + '˚', inline=False)  # 내일오후날씨
-        embed.add_field(name='내일 오후날씨상태, 미세먼지 상태', value=tomorrowAfterValue, inline=False)  # 내일오후 날씨상태
-
-
-
-        await client.send_message(message.channel,embed=embed)
-    
 client.run(access_token)
