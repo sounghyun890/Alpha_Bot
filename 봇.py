7ff62ccf04a7ce5d542ca8f4954671b88faa333b
Tanat <85154556+TANAT96564@users.noreply.github.com>
2021-07-13 10:02:15 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -10,40 +10,19 @@ riot_token = "rtoken"
 async def on_ready():
     print("--- ready ---")
 
+
 @client.event
 async def on_message(message):
-    if message.content.startswith("/검색 "):
-        UserName = message.content.replace("/검색 ", "")
-        UserInfoUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + UserName
-        res = requests.get(UserInfoUrl, headers={"X-Riot-Token":riot_token})
-        resjs = json.loads(res.text)
+    if message.content.startswith('씨'):
+        channel = message.channel
+        
 
-        if res.status_code == 200:
-            UserIconUrl = "http://ddragon.leagueoflegends.com/cdn/11.3.1/img/profileicon/{}.png"
-            embed = discord.Embed(title=f"{resjs['name']} 님의 플레이어 정보", description=f"**{resjs['summonerLevel']} LEVEL**", color=0xFF9900)
+        def check(m):
+            return m.content == '발' and m.channel == channel
 
-            UserInfoUrl_2 = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + resjs["id"]
-            res_2 = requests.get(UserInfoUrl_2, headers={"X-Riot-Token":riot_token})
-            res_2js = json.loads(res_2.text)
-
-            if res_2js == []: # 언랭크일때
-                embed.add_field(name=f"{resjs['name']} 님은 언랭크입니다.", value="**언랭크 유저의 정보는 출력하지 않습니다.**", inline=False)
-
-            else: # 언랭크가 아닐때
-                for rank in res_2js:
-                    if rank["queueType"] == "RANKED_SOLO_5x5":
-                        embed.add_field(name="솔로랭크", value=f"**티어 : {rank['tier']} {rank['rank']} - {rank['leaguePoints']} LP**\n"
-                                                           f"**승 / 패 : {rank['wins']} 승 {rank['losses']} 패**", inline=True)
-
-                    else:
-                        embed.add_field(name="자유랭크", value=f"**티어 : {rank['tier']} {rank['rank']} - {rank['leaguePoints']} LP**\n"
-                                                            f"**승 / 패 : {rank['wins']} 승 {rank['losses']} 패**", inline=True)
-
-            embed.set_author(name=resjs['name'], url=f"http://fow.kr/find/{UserName.replace(' ', '')}", icon_url=UserIconUrl.format(resjs['profileIconId']))
-            await message.channel.send(embed=embed)
-
-        else: # 존재하지 않는 소환사일때
-            error = discord.Embed(title="존재하지 않는 소환사명입니다.\n다시 한번 확인해주세요.", color=0xFF9900)
-            await message.channel.send(embed=error)
-
+        msg = await client.wait_for('message', check=check)
+        await channel.send('Hello {.author}!'.format(msg))
+        
+        
+        
 client.run(token)
