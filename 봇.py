9a3880642440d30e63fb953db438d68547f35de7
Tanat <85154556+TANAT96564@users.noreply.github.com>
2021-07-14 08:53:27 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -15,12 +15,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('씨'):
+    if message.content.startswith('!hello'):
         channel = message.channel
-        
+        await channel.send('hello라고 말해 주세요')
 
         def check(m):
-            return m.content == '발' and m.channel == channel
+            return m.content == 'hello' and m.channel == channel
 
         msg = await client.wait_for('message', check=check)
         await channel.send('Hello {.author}!'.format(msg))
